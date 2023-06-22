c82f5225f369cb1c3cc462e3cbec7626fd997c38
Rohit <rohit.lama@my.istec.pt>
2023-06-22 07:05:45 -0700

    Added the user input

---
+++ SG_ClinicasVeterinarias/pt.com.GCV.CONEXAO/SQLUser.cs
@@ -0,0 +1,349 @@
+﻿using SG_ClinicasVeterinarias.pt.com.GCV.MODEL;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Data.SqlClient;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using  static SG_ClinicasVeterinarias.pt.com.GCV.DAO.SqLConnection;
+using static SG_ClinicasVeterinarias.Setting.Utils;
+
+namespace SG_ClinicasVeterinarias.pt.com.GCV.CONEXAO
+{
+    internal class SQLUser
+    {
+        #region Create
+
+        /// <summary>
+        /// Adiciona um novo registo à tabela
+        /// </summary>
+        /// <param name="user"></param>
+        static public void Insert(User user)
+        {
+            try
+            {
+                using (DbConnection conn = OpenConnection())
+                {
+                    using (SqlCommand sqlCommand = ((SqlConnection)conn).CreateCommand())
+                    {
+                        sqlCommand.CommandType = CommandType.Text;
+                        sqlCommand.CommandText = "INSERT INTO \"Users\""
+                        + "(Name,Email,Password, CreatedAt)"
+                        + "VALUES(@name, @email, @password, @createdAt);";
+                        sqlCommand.Parameters.Add(new SqlParameter("@name", user.Name));
+                        sqlCommand.Parameters.Add(new SqlParameter("@email", user.Email));
+                        sqlCommand.Parameters.Add(new SqlParameter("@password", user.Password));
+                        sqlCommand.Parameters.Add(new SqlParameter("@createdAt", DateTime.Now));
+
+                        if (sqlCommand.ExecuteNonQuery() != 1)
+                        {
+                            throw new InvalidProgramException("SQLUser - add() - SQLServer Local: ");
+                        }
+                    }
+
+                }
+                //CloseAllConnections(true);
+            }
+            catch (Exception exeption)
+            {
+                MessageBox.Show(
+                    "SQLUser - Add() - \n The connection to the database was lost: \n" + exeption.ToString(),
+                    "SQLUser - Add() - Catch",   // Título
+                    MessageBoxButtons.OK,       // Botões
+                    MessageBoxIcon.Error  // Icon
+                );
+            }
+        }
+        #endregion
+
+        #region Read
+        static public List<User> GetAll()
+        {
+            List<User> users = new List<User>();   // Lista Principal
+            String query = "";
+
+
+            //Execução do SQL DML sob controlo do try catch
+            try
+            {
+                // Abre ligação ao DBMS Ativo
+                using (DbConnection conn = OpenConnection())
+                {
+                    query = "SELECT * FROM Users;";
+
+                    using (SqlCommand sqlCommand = new SqlCommand())
+                    {
+                        sqlCommand.CommandText = query;
+                        sqlCommand.CommandType = CommandType.Text;
+                        sqlCommand.Connection = ((SqlConnection)conn);
+
+                        using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                        {
+
+                            while (reader.Read())
+                            {
+                                User user = new User(
+                                    reader.GetInt32(reader.GetOrdinal("ID")),
+                                    reader["Name"].ToString(),
+                                    reader["Email"].ToString(),
+                                    reader["Password"].ToString());
+
+                                users.Add(user);
+                            }
+                        }
+                    }
+
+                }
+
+                // adding relationship if exists
+                //TODO add the relationship here with the client
+                //foreach (User user in users)
+                //{
+                //    User.Client = SQLClient.Get(Convert.ToInt32(User.Client.Id));
+
+                //}
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: SQLuser - getAll() - \n" + e.ToString());
+                MessageBox.Show(
+                    "SQLuser - GetAll() - \n Ocorreram problemas com a ligação à Base de Dados: \n" + e.ToString(),
+                    "SQLuser - GetAll() - Catch",   // Título
+                    MessageBoxButtons.OK,       // Botões
+                    MessageBoxIcon.Error  // Icon
+                );
+                return null;
+            }
+            return users;
+        }
+
+        /// <summary>
+        /// Obtem um registo completo da tabela através do seu id, converte para obj e devolve.
+        /// Há várias ligações - Processo:
+        /// 1 - Ligação BD - Extrai o registo BD.
+        /// 2 - Completa os objeto da lista principal, preenchendo os obj FK, com base nas listas FK
+        ///     Usa um ciclo para correr a lista principal e um ciclo ou obj FK
+        /// </summary>
+        /// <returns>Devolve um objeto preenchido ou NULL</returns>
+        static public User GetById(int Id)
+        {
+            User user = null;
+
+            try
+            {
+                using (DbConnection conn = OpenConnection())
+                {
+                    using (SqlCommand sqlCommand = new SqlCommand())
+                    {
+                        sqlCommand.CommandType = CommandType.Text;
+                        sqlCommand.Connection = ((SqlConnection)conn);
+
+                        sqlCommand.CommandText = "SELECT * FROM Users where ID=@id;";
+                        sqlCommand.Parameters.Add(new SqlParameter("@id", Id));
+
+                        using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                user = new User(
+                                    reader.GetInt32(reader.GetOrdinal("ID")),
+                                    reader["Name"].ToString(),
+                                    reader["Email"].ToString(),
+                                    reader["Password"].ToString());
+
+                            }
+                        }
+
+                        //TODO add relationship here
+                        //User.Client = SQLClient.GetClientById(User.Client.Id);
+                    }
+                }
+
+                // Se Entidade tem FKs, completar o objeto extraído com o objeto fk, com um get(id) à SQL respetiva
+                //User.CategoriaUser = SQLcategoriaUser.Get(Convert.ToInt32(User.CategoriaUser.ID));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: SQLUser - get() - \n" + e.ToString());
+                MessageBox.Show(
+                    "SQLUser - Get() - \n Ocorreram problemas com a ligação à Base de Dados: \n" + e.ToString(),
+                    "SQLUser - Get() - Catch",   // Título
+                    MessageBoxButtons.OK,       // Botões
+                    MessageBoxIcon.Error  // Icon
+                );
+                return null;
+            }
+            return user;
+        }
+
+        public static User GetUserByEmailAndPassword(User user)
+        {
+            try
+            {
+                using (DbConnection conn = OpenConnection())
+                {
+                    using (SqlCommand sqlCommand = new SqlCommand())
+                    {
+                        sqlCommand.CommandType = CommandType.Text;
+                        sqlCommand.Connection = ((SqlConnection)conn);
+
+                        sqlCommand.CommandText = "SELECT * FROM Users " +
+                                                 "Where Email=@email AND Password=@password;";
+                        sqlCommand.Parameters.Add(new SqlParameter("@email", user.Email));
+                        sqlCommand.Parameters.Add(new SqlParameter("@password", user.Password));
+
+                        using (SqlDataReader reader = sqlCommand.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                user = new User(
+                                    reader.GetInt32(reader.GetOrdinal("ID")),
+                                    reader["Name"].ToString(),
+                                    reader["Email"].ToString(),
+                                    reader["Password"].ToString());
+
+                            }
+                            else
+                            {
+                                return null;
+                            }
+                        }
+
+                        //TODO add relationship here
+                        //User.Client = SQLClient.GetClientById(User.Client.Id);
+                    }
+                }
+
+                // Se Entidade tem FKs, completar o objeto extraído com o objeto fk, com um get(id) à SQL respetiva
+                //User.CategoriaUser = SQLcategoriaUser.Get(Convert.ToInt32(User.CategoriaUser.ID));
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: SQLUser - get() - \n" + e.ToString());
+                MessageBox.Show(
+                    "SQLUser - Get() - \n Ocorreram problemas com a ligação à Base de Dados: \n" + e.ToString(),
+                    "SQLUser - Get() - Catch",   // Título
+                    MessageBoxButtons.OK,       // Botões
+                    MessageBoxIcon.Error  // Icon
+                );
+                return null;
+            }
+            return user;
+        }
+
+        #endregion
+
+        #region Update
+        static public void Update(User user)
+        {
+            try
+            {
+                using (DbConnection conn = OpenConnection())
+                {
+                    switch (DBMSactive)
+                    {
+                        case DBMS_BD_LOCAL:
+
+                            using (SqlCommand sqlCommand = ((SqlConnection)conn).CreateCommand())
+                            {
+                                sqlCommand.CommandType = CommandType.Text;
+                                sqlCommand.CommandText = "UPDATE Users SET" +
+                                    " Name = @name," +
+                                    " Email = @email," +
+                                    " password = @password" +
+                                    " WHERE ID = @id";
+                                sqlCommand.Parameters.Add(new SqlParameter("@id", user.Id));
+                                sqlCommand.Parameters.Add(new SqlParameter("@name", user.Name));
+                                sqlCommand.Parameters.Add(new SqlParameter("@email", user.Email));
+                                sqlCommand.Parameters.Add(new SqlParameter("@password", user.Password));
+
+                                // Tenta executar o comando, que é suposto devolver 1
+                                if (sqlCommand.ExecuteNonQuery() != 1)
+                                {
+                                    // Se diferente, inverte o commit e Provoca a excessão saltanto para o catch
+                                    throw new InvalidProgramException("SQLUser - set() - bdLocal: ");
+                                }
+                            }
+                            break;
+
+
+                        default:
+                            MessageBox.Show(
+                                "O DBMS Ativo não está definido: SQLUser - update()",
+                                "SQLuser - Set() - switch(DBMSactive)",   // Título
+                                MessageBoxButtons.OK,       // Botões
+                                MessageBoxIcon.Information  // Icon
+                            );
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: SQLUser - update() - \n" + e.ToString());
+                MessageBox.Show(
+                    "SQLUser - update() - \n Ocorreram problemas com a ligação à Base de Dados: \n" + e.ToString(),
+                    "SQLUser - update() - Catch",   // Título
+                    MessageBoxButtons.OK,       // Botões
+                    MessageBoxIcon.Error  // Icon
+                );
+            }
+        }
+        #endregion
+
+        #region Delete
+        static public void Delete(User user)
+        {
+            try
+            {
+                using (DbConnection conn = OpenConnection())
+                {
+                    switch (DBMSactive)
+                    {
+                        case DBMS_BD_LOCAL:
+                            using (SqlCommand sqlCommand = ((SqlConnection)conn).CreateCommand())
+                            {
+                                sqlCommand.CommandType = CommandType.Text;
+                                sqlCommand.CommandText = "DELETE FROM Users WHERE ID=@id ;";
+                                sqlCommand.Parameters.Add(new SqlParameter("@id", user.Id));
+
+                                // Tenta executar o comando, que é suposto devolver 1
+                                if (sqlCommand.ExecuteNonQuery() != 1)
+                                {
+                                    // Se diferente, inverte o commit e Provoca a excessão saltanto para o catch
+                                    throw new InvalidProgramException("SQLUser - delelete() - bdlocal: ");
+                                }
+                            }
+                            break;
+                        default:
+                            MessageBox.Show(
+                                "O DBMS Ativo não está definido: SQLUser - delete()",
+                                "SQLUser - Delete() - switch(DBMSactive)",   // Título
+                                MessageBoxButtons.OK,       // Botões
+                                MessageBoxIcon.Information  // Icon
+                            );
+                            break;
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Erro: SQLUser - delete() - \n" + e.ToString());
+                MessageBox.Show(
+                    "SQLUser - delete() - \n Ocorreram problemas com a ligação à Base de Dados: \n" + e.ToString(),
+                    "SQLUser - delete() - Catch",   // Título
+                    MessageBoxButtons.OK,       // Botões
+                    MessageBoxIcon.Error  // Icon
+                );
+            }
+        }
+
+
+        #endregion
+
+
+    }
+}
---
+++ SG_ClinicasVeterinarias/pt.com.GCV.MODEL/User.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SG_ClinicasVeterinarias.pt.com.GCV.MODEL
+{
+    internal class User
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Default constructor for instanciate
+        /// </summary>
+        public User()
+        {
+
+        }
+
+        /// <summary>
+        /// Id for get by id sql query 
+        /// </summary>
+        /// <param name="id"></param>
+        public User(int id)
+        {
+            Id = id;
+        }
+
+        /// <summary>
+        /// login Constructor
+        /// </summary>
+        /// <param name="email"></param>
+        /// <param name="password"></param>
+        public User(string email, string password)
+        {
+            Email = email;
+            Password = password;
+        }
+
+        /// <summary>
+        /// full constructor to instance a complete object
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="age"></param>
+        /// <param name="birthDate"></param>
+        public User(int id, string name, string email, string password)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+            Password = password;
+        }
+    }
+}
